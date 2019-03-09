eada366a072ee871d802f2d860ffaa4035c342ab
imsat <worksatyajit@gmail.com>
2019-03-09 23:12:15 +0600

    Initial Commit Develop Branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Vue Datatable</div>
+                    <div class="card-header">Vue Datatable v0.1</div>
 
                     <div class="card-body">
                         <app-user></app-user>
