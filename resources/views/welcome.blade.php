bcaaa83ea15e6bc4b9b564cf6fcfc9cbf99ea37b
imsat <worksatyajit@gmail.com>
2019-03-10 23:26:44 +0600

    full completed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Vue Datatable Develop</div>
+                    <div class="card-header">Vue Datatable Client Side</div>
 
                     <div class="card-body">
                         <app-user-client-side></app-user-client-side>
